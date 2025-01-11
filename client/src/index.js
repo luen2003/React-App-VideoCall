eb31941dafbf3a5dbd104118ad0a48ddc2c2ef82
21020776TaDinhLuong <21020776@vnu.edu.vn>
2025-01-11 10:34:42 +0700

    add pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';  // Use react-dom, not react-dom/client
-import App from './js/App';
+import App from './App';
 import './css/app.scss';
+import { Provider } from 'react-redux';
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
--- client/webpack-prod.config.js
+++ client/webpack-prod.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
@@ -9,7 +10,9 @@ module.exports = {
     app: './src/index.js'
   },
   output: {
-    filename: 'js/[name].min.js'
+    publicPath: '/',  // Webpack will use this to reference assets
+    path: path.resolve(__dirname, 'dist'),  // Output directory
+    filename: 'js/[name].[contenthash].js',  // Output JS file with hash for cache busting
   },
   module: {
     rules: [
@@ -30,32 +33,26 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
+          MiniCssExtractPlugin.loader,  // Extract CSS into separate files
+          'css-loader',  // Load CSS files
+          'sass-loader'  // Compile SCSS to CSS
         ]
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets',
-              publicPath: '/assets'
-            }
-          }
-        ]
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/i,
+        type: 'asset/resource',  // Use asset modules for images, fonts, etc.
+        generator: {
+          filename: 'assets/[name].[hash][ext][query]',  // Output asset files
+        }
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'css/[name].min.css' }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' }),  // Output CSS to 'css' folder with hash
     new HtmlWebpackPlugin({
       title: 'React VideoCall',
       filename: 'index.html',
-      template: 'src/html/index.html'
+      template: 'src/html/index.html',  // Use the template for generating HTML file
     })
   ],
   optimization: {
